6c3421bce97044be6cb9bea00eb36a1d29405280
Bahul Neel Upadhyaya <bahulneel@gmail.com>
2017-03-24 18:49:40 +0000

    Initial schema all in one file (incomplete)

--- project.clj
+++ project.clj
@@ -3,12 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]]
-  :profiles {:dev {:dependencies [[com.datomic/datomic-free "0.9.5561"
-                                   :exclusions [org.slf4j/slf4j-api
-                                                org.slf4j/slf4j-nop
-                                                org.slf4j/slf4j-log4j12
-                                                org.slf4j/log4j-over-slf4j
-                                                org.slf4j/jcl-over-slf4j
-                                                org.slf4j/jul-to-slf4j
-                                                org.clojure/clojure]]]}})
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [com.datomic/datomic-free "0.9.5561"
+                  :exclusions [org.clojure/clojure]
+                  :scope "test"]])
--- src/epistomic/core.clj
+++ src/epistomic/core.clj
@@ -1,5 +1,5 @@
-(ns epistomic.core)
-
+(ns epistomic.core
+  (:require [datomic.api :as d]))
 
 (defn foo
   "I don't do a whole lot."
