09904b65224aefb5af2d8281c72380240bda4b8d
Bahul Neel Upadhyaya <bahulneel@gmail.com>
2017-03-24 16:52:55 +0000

    Require datomic latest

--- project.clj
+++ project.clj
@@ -2,5 +2,13 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]]
+  :profiles {:dev {:dependencies [[com.datomic/datomic-free "0.9.5561"
+                                   :exclusions [org.slf4j/slf4j-api
+                                                org.slf4j/slf4j-nop
+                                                org.slf4j/slf4j-log4j12
+                                                org.slf4j/log4j-over-slf4j
+                                                org.slf4j/jcl-over-slf4j
+                                                org.slf4j/jul-to-slf4j
+                                                org.clojure/clojure]]]}})
